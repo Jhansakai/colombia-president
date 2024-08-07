a38d03d3e9d0ef61f3e6e82eb7ef62a3f565804e
YursKsf1 <yurs.ksf@gmail.com>
2024-08-06 19:36:01 -0500

    definir modelos y admin

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from myapp.models import Partido, Presidente
 
 # Register your models here.
+
+admin.site.register(Partido)
+admin.site.register(Presidente)
--- myapp/models.py
+++ myapp/models.py
@@ -1,4 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Partido(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    
+    def __str__(self):
+       return "{}".format(self.name)
 
+
+class Presidente(models.Model):
+    numero = models.IntegerField()
+    name = models.CharField(max_length=60)
+    partido = models.ForeignKey(Partido, on_delete=models.CASCADE)
+    inicio = models.IntegerField()
+    final = models.IntegerField()
+    description = models.TextField()
+    
+    def __str__(self):
+       return "{}".format(self.name)
+    
