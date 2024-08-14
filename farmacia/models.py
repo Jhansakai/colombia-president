deec525dfa234808c4b89a196d2564bd49bbd50e
Jhansakai <jhans.akai@gmail.com>
2024-08-13 19:46:33 -0500

    cambios

--- farmacia/models.py
+++ farmacia/models.py
@@ -1,45 +1,19 @@
 from django.db import models
 
-class Marca(models.Model):
+class Categoria(models.Model):
     name = models.CharField(max_length=60)
-    description = models.TextField()
+    descripcion = models.TextField()
     
     def __str__(self):
        return "{}".format(self.name)
 
-class Vitaminas(models.Model):
+class Producto(models.Model):
     name = models.CharField(max_length=200)
-    marca = models.ForeignKey(Marca, on_delete=models.CASCADE)
+    marca = models.CharField(max_length=100)
     lote = models.IntegerField()
     fecha_vencimiento = models.IntegerField()
-    
+    descripcion =  models.TextField()
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+
     def __str__(self):
        return "{}".format(self.name)
-
-class Analgesicos(models.Model):
-    name = models.CharField(max_length=200)
-    marca = models.ForeignKey(Marca, on_delete=models.CASCADE)
-    lote = models.IntegerField()
-    fecha_vencimiento = models.IntegerField()
-    
-    def __str__(self):
-       return "{}".format(self.name)
-    
-class Antibiotico(models.Model):
-    name = models.CharField(max_length=200)
-    marca = models.ForeignKey(Marca, on_delete=models.CASCADE)
-    lote = models.IntegerField()
-    fecha_vencimiento = models.IntegerField()
-    
-    def __str__(self):
-       return "{}".format(self.name)    
-    
-class Antivirales(models.Model):
-    name = models.CharField(max_length=200)
-    marca = models.ForeignKey(Marca, on_delete=models.CASCADE)
-    lote = models.IntegerField()
-    fecha_vencimiento = models.IntegerField()
-    
-    def __str__(self):
-       return "{}".format(self.name)    
-
--- farmacia/views.py
+++ farmacia/views.py
@@ -1,3 +1,67 @@
 from django.shortcuts import render
-
+from farmacia.models import Producto, Categoria
 # Create your views here.
+def list_categorias(request):
+    categorias = Categoria.objects.all()
+    context = {
+        "categorias": categorias
+    }
+    return render(request, 'myapp/list-categorias.html', context)
+
+def list_productos(request):
+    productos = []
+    filtro = request.GET.get('search')
+    if filtro:
+        productos = Producto.objects.filter(name__icontains=filtro).all()
+    else:
+        productos = Producto.objects.all()
+        context = {
+            "productos": productos
+        }
+        return render(request, 'myapp/list-productos.html', context)
+
+def detail_productos(request, id):
+    producto = Producto.objects.filter(code=id).first()
+    context = {
+        "producto": producto
+    }
+    return render(request, 'myapp/detail-productos.html', context)
+
+def delete_productos(request, id):
+    producto = Producto.objects.filter(code=id).first()
+    producto.delete()
+
+    productos = Producto.objects.all()
+    context = {
+        "productos": productos
+    }
+    return render(request, 'myapp/list-productos.html', context)
+
+def update_productos(request, id):
+    producto = Producto.objects.filter(code=id).first()
+    method = request.method
+    if method == 'POST':
+        data = request.POST
+        name = data.get('name')
+        description = data.get('description')
+
+        producto.name = name
+        producto.description = description
+        producto.save()
+
+    context = {
+        "producto": producto
+    }
+    return render(request, 'myapp/update-productos.html', context)
+
+def index(request):
+    context = {
+        
+    }
+    return render(request, 'myapp/index.html', context)
+
+def dashboard(request):
+    context = {
+        
+    }
+    return render(request, 'myapp/dashboard.html', context)
